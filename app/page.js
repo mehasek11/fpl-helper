909d3669bc7ecd61bf72ff46bb41ab1b5b76c806
mehasek11 <byg26ryhh8@privaterelay.appleid.com>
2026-08-23 20:10:29 +0300

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '40px', fontFamily: 'sans-serif' }}>
-      <h1>FPL Helper API is Live</h1>
-      <p>Access the proxy at <a href="/api/fpl-proxy">/api/fpl-proxy</a></p>
-    </main>
-  );
-}
